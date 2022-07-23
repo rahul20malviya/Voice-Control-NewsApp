14079d8d286cbafe6a7713e91d8ddba83320ca9d
Rahul Malviya <rahul20malviya@gmail.com>
2022-07-24 01:07:11 +0530

    update files

---
+++ src/Service/alanScript.js
@@ -0,0 +1,9 @@
+intent('$(item* (.*))', (p) => {
+    if(p.item.value === 'health' || p.item.value === 'Sports' || p.item.value === 'business' || p.item.value === 'General' ){
+        p.play({ command: 'VoiceNews', data:p.item.value});
+        p.play(`Fetching News on ${p.item.value} Category`);
+    }
+    else{
+        p.play(`Cannot get Data`);
+    }
+})
